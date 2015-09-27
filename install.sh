0248531a8749bc8872f668f947c9a479c6517464
ayberkozgur <ayberk.ozgur@epfl.ch>
2015-09-27 19:52:07 +0300

    Updated ycm

--- install.sh
+++ install.sh
@@ -30,30 +30,6 @@ check_deps(){
     fi
 }
 
-build_ycm(){
-    RETVAL=0
-
-    #Try to create build directory
-    rm -rf build/ || return 1
-    mkdir build/ || return 1
-    cd build/
-    if [ "$?" -ne "0" ]
-    then
-        rmdir build/
-        return 1
-    fi
-
-    #Build
-    cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON . "$HOME/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp" || RETVAL=1
-    make ycm_support_libs -j5 || RETVAL=1
-
-    #Cleanup build directory
-    cd .. || RETVAL=1
-    rm -rf build/ || RETVAL=1
-
-    return "$RETVAL"
-}
-
 #Get -f argument
 if [ "$1" == "-f" ]
 then
@@ -89,6 +65,6 @@ echo -e "$INITCMD: Getting ~/.vimrc in place...done."
 #Build ycm
 echo -e "$INITCMD: Building YouCompleteMe..."
 cd "$HOME/.vim/bundle/YouCompleteMe" || exit 1
-build_ycm || error_and_die "$INITCMDERR: Failed to build YouCompleteMe."
+./install.py --clang-completer || error_and_die "$INITCMDERR: Failed to build YouCompleteMe."
 echo -e "$INITCMD: Building YouCompleteMe...done."
 
