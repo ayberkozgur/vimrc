ee5058085e6916fbf7e89a4c3a139193b4355bb0
ayberkozgur <ayberk.ozgur@epfl.ch>
2015-09-28 11:36:07 +0300

    Added symlink install script, nvim symlinks

--- install.sh
+++ install.sh
@@ -30,6 +30,25 @@ check_deps(){
     fi
 }
 
+symlink(){
+    FROM=$1
+    TO=$2
+    POSSIBLY_FROM=$3
+
+    echo -e "$INITCMD: Getting $TO in place..."
+    ln -s $LNARG $FROM $TO
+    if [ "$?" -ne "0" ]
+    then
+        if [ "$(readlink -f $TO)" == $FROM -o "$(readlink -f $TO)" == $POSSIBLY_FROM ]
+        then
+            echo -e "$INITCMD: $TO is already installed, skipping."
+        else
+            error_and_die "$INITCMDERR: $TO already exists as something else; run with -f to overwrite target files"
+        fi
+    fi
+    echo -e "$INITCMD: Getting $TO in place...done."
+}
+
 #Get -f argument
 if [ "$1" == "-f" ]
 then
@@ -48,19 +67,10 @@ echo -e "$INITCMD: Getting plugins..."
 git submodule update --init --recursive || exit 1
 echo -e "$INITCMD: Getting plugins...done."
 
-#Symlink ~/.vimrc
-echo -e "$INITCMD: Getting ~/.vimrc in place..."
-ln -s $LNARG "$HOME/.vim/vimrc" "$HOME/.vimrc"
-if [ "$?" -ne "0" ]
-then
-    if [ "$(readlink -f "$HOME/.vimrc")" == "$HOME/.vim/vimrc" ]
-    then
-        echo -e "$INITCMD: ~/.vimrc is already installed, skipping."
-    else
-        error_and_die "$INITCMDERR: ~/.vimrc already exists as something else; run with -f to overwrite target files"
-    fi
-fi
-echo -e "$INITCMD: Getting ~/.vimrc in place...done."
+#Symlink vimrc, nvim, nvimrc
+symlink "$HOME/.vim/vimrc" "$HOME/.vimrc" "$HOME/.dotfiles/vim/vimrc"
+symlink "$HOME/.vim" "$HOME/.nvim" "$HOME/.dotfiles/nvim"
+symlink "$HOME/.vim/vimrc" "$HOME/.nvimrc" "$HOME/dotsfiles/vim/vimrc"
 
 #Build ycm
 echo -e "$INITCMD: Building YouCompleteMe..."
